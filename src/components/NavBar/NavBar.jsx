e7ab980e8051fd64117a07b61669eb14ca84097c
Gonzalo <fonsofara@gmail.com>
2022-10-12 01:26:16 -0300

    cv download

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { RiArrowDownLine } from "react-icons/ri";
 
 const NavBar = () => {
   return (
@@ -29,6 +30,15 @@ const NavBar = () => {
           className="border-b-2 border-transparent hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6"
         >
           Contact
+        </a>
+        <a
+          href="../../CV - Gonzalo Fara.pdf"
+          className="border-b-2 border-transparent hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6"
+        >
+          <span className="flex items-center gap-px">
+            <p>CV</p>
+            <RiArrowDownLine />
+          </span>
         </a>
       </div>
     </nav>
